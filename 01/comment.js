f5c9bb310ec94ac0def93980fbba2a6de3c4997b
aji prasetyo <ajiprxty3@gmail.com>
2024-10-04 20:35:47 +0700

    update

---
+++ 01/comment.js
@@ -0,0 +1,14 @@
+// single comment
+// console.log('baca console');
+
+// multi line comment
+/* console.log('baca console');
+console.log('baca console');
+console.log('baca console'); */
+
+console.log('baca console');
+
+const pi = 3.14;
+const radius = 10;
+const area = pi * radius * radius;
+console.log(`The area of a circle with radius ${radius} is ${area}.`);
---
+++ 01/variabel.js
@@ -0,0 +1,40 @@
+const name = 'Aji'; // tidak dapat diubah
+
+let fullName = 'Aji'; // dapat diubah
+
+fullName = 'Aji Prasetyo';
+
+console.log(fullName);
+
+function companyInfo(){
+    const companyName = 'Eureka';
+    const legal = 'PT. Eureka Indonesia';
+
+    console.log(`Company Name: ${companyName}, Legal: ${legal}`);
+}
+
+companyInfo();
+
+// nama variabel yang benar
+const firstName = 'Fulan';
+const last_name = 'Lestari';
+const $message = 'Hello, World!';
+const userId1 = 123;
+const userId2 = 456;
+ 
+// nama variabel yang salah
+// const first-name = 'Fulan'; // tidak boleh mengandung karakter -
+// const last name = 'Lestari'; // tidak boleh mengandung spasi
+// const @message = 'Hello, World!'; // tidak boleh mengandung karakter @
+ 
+
+// nama variabel yang benar
+// const firstName = 'Fulan';
+const _secondName = 'Fulana';
+ 
+// nama variabel yang salah karena diawali dengan angka
+// const 1stName = 'Fulan';
+// const 2ndName = 'Fulana';
+
+
+
